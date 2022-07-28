5dbb7149714a21271186ac2a357ebf1a3d9b0c6c
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-07-28 12:15:30 +0200

    Feat: Registration Form - Reactive Forms

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit {
-  constructor() {}
+export class RegisterComponent {
+  public registerForm = this.fb.group({
+    nombre: ['Manuel', [Validators.required]],
+    email: ['test100@gmail.com', [Validators.required]],
+    password: ['secretisima', [Validators.required]],
+    password2: ['secretisima', [Validators.required]],
+    terminos: [false, [Validators.required]],
+  });
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {}
+  crearUsuario() {
+    console.log(this.registerForm.value);
+  }
 }
