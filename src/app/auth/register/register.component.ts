67bababe02cb96fe420daa22b391b167342307c2
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-07-28 13:13:03 +0200

    Feat: Validaciones del formulario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -7,16 +7,34 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  public formSubmitted = false;
+
   public registerForm = this.fb.group({
     nombre: ['Manuel', [Validators.required]],
-    email: ['test100@gmail.com', [Validators.required]],
-    password: ['secretisima', [Validators.required]],
-    password2: ['secretisima', [Validators.required]],
+    email: ['test@test.com', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    password2: ['', [Validators.required]],
     terminos: [false, [Validators.required]],
   });
   constructor(private fb: FormBuilder) {}
 
   crearUsuario() {
+    this.formSubmitted = true;
     console.log(this.registerForm.value);
+    if (this.registerForm.valid) {
+      console.log('posteando formulario');
+    } else {
+      console.log('fromulario incorrecto');
+    }
+  }
+  campoNoValido(campo: string): boolean {
+    if (this.registerForm.get(campo).invalid && this.formSubmitted) {
+      return true;
+    } else {
+      false;
+    }
+  }
+  aceptaTerminos() {
+    return this.registerForm.get('terminos').value && this.formSubmitted;
   }
 }
