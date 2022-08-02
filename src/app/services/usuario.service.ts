56ebe2ef7d7e2f40afa1cef995593180337e1649
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-08-02 11:55:33 +0200

    Feat: Save information to LocalStorage

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { RegisterForm } from '../interfaces/register-form.interface';
-import { LoginForm } from '../interfaces/login-form.interface';
+import { tap } from 'rxjs/operators';
 
 import { environment } from 'src/environments/environment';
+
+import { RegisterForm } from '../interfaces/register-form.interface';
+import { LoginForm } from '../interfaces/login-form.interface';
 
 const base_url = environment.base_url;
 
@@ -14,10 +16,18 @@ export class UsuarioService {
   constructor(private htpp: HttpClient) {}
 
   crearUsuario(formData: RegisterForm) {
-    return this.htpp.post(`${base_url}/usuarios`, formData);
+    return this.htpp.post(`${base_url}/usuarios`, formData).pipe(
+      tap((resp: any) => {
+        localStorage.setItem('token', resp.token);
+      })
+    );
   }
 
   login(formData: LoginForm) {
-    return this.htpp.post(`${base_url}/login`, formData);
+    return this.htpp.post(`${base_url}/login`, formData).pipe(
+      tap((resp: any) => {
+        localStorage.setItem('token', resp.token);
+      })
+    );
   }
 }
