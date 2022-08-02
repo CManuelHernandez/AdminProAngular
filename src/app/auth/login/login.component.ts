bed8d26bd0e4b1a48e12dea89c4485cd9f89bc6e
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-08-02 11:23:31 +0200

    Feat: User login - normal

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,17 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+
 import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
-  constructor(private router: Router) {}
+export class LoginComponent {
+  public formSubmitted = false;
 
-  ngOnInit(): void {}
+  public loginForm = this.fb.group({
+    email: ['test@test.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required]],
+    remember: [false],
+  });
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private usuarioService: UsuarioService
+  ) {}
 
   login() {
-    this.router.navigateByUrl('/');
+    this.usuarioService.login(this.loginForm.value).subscribe(
+      (resp) => {
+        console.log(resp);
+      },
+      (err) => {
+        // Si sucede un error
+        Swal.fire('Error', err.error.msg, 'error');
+      }
+    );
+
+    //this.router.navigateByUrl('/');
   }
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RegisterForm } from '../interfaces/register-form.interface';
+import { LoginForm } from '../interfaces/login-form.interface';
+
 import { environment } from 'src/environments/environment';
 
 const base_url = environment.base_url;
@@ -13,5 +15,9 @@ export class UsuarioService {
 
   crearUsuario(formData: RegisterForm) {
     return this.htpp.post(`${base_url}/usuarios`, formData);
+  }
+
+  login(formData: LoginForm) {
+    return this.htpp.post(`${base_url}/login`, formData);
   }
 }
