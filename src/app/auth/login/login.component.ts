be1b2aeaf201a06168cd3bce93e422750645b8c7
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-08-04 10:52:27 +0200

    Feat: Obtain the Token of an authentication, Google Sign-In

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
 
 import { Router } from '@angular/router';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
+declare const google: any;
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
+  @ViewChild('googleBtn') googleBtn: ElementRef;
+
   public formSubmitted = false;
 
   public loginForm = this.fb.group({
@@ -26,6 +30,26 @@ export class LoginComponent {
     private fb: FormBuilder,
     private usuarioService: UsuarioService
   ) {}
+
+  ngAfterViewInit(): void {
+    this.googleInit();
+  }
+
+  googleInit() {
+    google.accounts.id.initialize({
+      client_id:
+        '31828481885-pnjvk3h1cdogr7vte07dhvgnqk89dmsg.apps.googleusercontent.com',
+      callback: this.handleCredentialResponse,
+    });
+    google.accounts.id.renderButton(
+      //document.getElementById('buttonDiv'),
+      this.googleBtn.nativeElement,
+      { theme: 'outline', size: 'large' } // customization attributes
+    );
+  }
+  handleCredentialResponse(response: any) {
+    console.log('Encoded JWT ID token: ' + response.credential);
+  }
 
   login() {
     this.usuarioService.login(this.loginForm.value).subscribe(
