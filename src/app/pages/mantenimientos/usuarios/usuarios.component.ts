528aa17b4526d0ce6a9a5ea913b89cf66795e96a
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-08-16 11:49:33 +0200

    Feat: Paginate the users

--- src/app/pages/mantenimientos/usuarios/usuarios.component.ts
+++ src/app/pages/mantenimientos/usuarios/usuarios.component.ts
@@ -10,13 +10,30 @@ import { UsuarioService } from 'src/app/services/usuario.service';
 export class UsuariosComponent implements OnInit {
   public totalUsuarios: number = 0;
   public usuarios: Usuario[] = [];
+  public desde: number = 0;
 
   constructor(private usuarioService: UsuarioService) {}
 
   ngOnInit(): void {
-    this.usuarioService.cargarUsuarios(0).subscribe(({ total, usuarios }) => {
-      this.totalUsuarios = total;
-      this.usuarios = usuarios;
-    });
+    this.cargarUsuarios();
+  }
+
+  cargarUsuarios() {
+    this.usuarioService
+      .cargarUsuarios(this.desde)
+      .subscribe(({ total, usuarios }) => {
+        this.totalUsuarios = total;
+        this.usuarios = usuarios;
+      });
+  }
+
+  cambiarPagina(valor: number) {
+    this.desde += valor;
+    if (this.desde < 0) {
+      this.desde = 0;
+    } else if (this.desde >= this.totalUsuarios) {
+      this.desde -= valor;
+    }
+    this.cargarUsuarios();
   }
 }
