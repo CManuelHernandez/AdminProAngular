37e4509e23c1a6396daa75183e728efc9c21820e
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-08-18 10:46:51 +0200

    Feat: Information loading message

--- src/app/pages/mantenimientos/usuarios/usuarios.component.ts
+++ src/app/pages/mantenimientos/usuarios/usuarios.component.ts
@@ -11,6 +11,7 @@ export class UsuariosComponent implements OnInit {
   public totalUsuarios: number = 0;
   public usuarios: Usuario[] = [];
   public desde: number = 0;
+  public cargando: boolean = true;
 
   constructor(private usuarioService: UsuarioService) {}
 
@@ -19,11 +20,13 @@ export class UsuariosComponent implements OnInit {
   }
 
   cargarUsuarios() {
+    this.cargando = true;
     this.usuarioService
       .cargarUsuarios(this.desde)
       .subscribe(({ total, usuarios }) => {
         this.totalUsuarios = total;
         this.usuarios = usuarios;
+        this.cargando = false;
       });
   }
 
